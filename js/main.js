9bc41c4496c0d83d86f9c514c00db12e7c2ccc83
David Fox Powell <dtothefp@gmail.com>
2014-01-18 15:45:38 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+  console.log("LOADED");
+});
+
+var nRelate = {
+  insert_html : function(response) {
+    var responseHolders = ["a", "b", "c"];
+    for(var i = 0; i < responseHolders.length; i++) {
+      var div = ($("<div>"))
+                  .addClass("response-" + responseHolders[i])
+                  .append($(response));
+      $(".nrelate-placeholder").append(div);
+      $(".response-a .nr_source").remove();
+      parseString(".nr_post_title");
+    }
+  }
+}
+
+var parseString = function(element) {
+  var elementArr = $(element);
+  _.each(elementArr, function(htmlElement){
+    var text = $(htmlElement).text();
+    if ( text.length >= 35 ) {
+      var parsedText = text.substr(0, 35) + "...";
+      console.log(parsedText);
+      $(htmlElement).text(parsedText);
+    } 
+  });
+}
+
+$.ajax({
+    cache: false,
+    dataType: "jsonp", 
+    type: "GET",
+    crossDomain: true,
+    jsonp: true,  
+    url: "http://api.nrelate.com/common/frontend_test_response.php",
+    async: true, // tried true
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+         console.log(textStatus, errorThrown);
+    }
+});
+
+
